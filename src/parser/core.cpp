66cdd4148d10c2cabc37a89ba4056be352a8653b
root <root@DESKTOP-04DSEHM.localdomain>
2023-03-02 18:15:19 +0900

    implement core parse

---
+++ src/parser/core.cpp
@@ -0,0 +1,72 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+class ParseError : public std::exception {
+    std::string message;
+
+   public:
+    ParseError(const std::string& msg) : message(msg) {}
+
+    const char* what() const noexcept override { return message.c_str(); }
+};
+
+std::vector<std::string> _tokenize(std::istream& input_file) {
+    std::vector<std::string> tokens;
+    std::string line;
+    while (std::getline(input_file, line)) {
+        // Strip comments.
+        line = line.substr(0, line.find(';'));
+
+        // Replace parentheses and question marks with spaces.
+        std::replace(line.begin(), line.end(), '(', ' ');
+        std::replace(line.begin(), line.end(), ')', ' ');
+        std::replace(line.begin(), line.end(), '?', ' ');
+
+        std::istringstream iss(line);
+        std::string token;
+        while (iss >> token) {
+            std::transform(token.begin(), token.end(), token.begin(),
+                           ::tolower);
+            tokens.push_back(token);
+        }
+    }
+    return tokens;
+}
+
+std::vector<std::string> _parse_list_aux(
+    std::vector<std::string>::iterator& it,
+    const std::vector<std::string>::iterator& end) {
+    std::vector<std::string> result;
+    while (it != end) {
+        const std::string& token = *it;
+        ++it;
+        if (token == ")") {
+            return result;
+        } else if (token == "(") {
+            std::vector<std::string> tmp = _parse_list_aux(it, end);
+            result.insert(result.end(), tmp.begin(), tmp.end());
+        } else {
+            result.push_back(token);
+        }
+    }
+    throw ParseError("Missing closing parenthesis");
+}
+
+std::vector<std::string> parse_nested_list(std::istream& input_file) {
+    std::vector<std::string> tokens = _tokenize(input_file);
+    if (tokens.empty() || tokens[0] != "(") {
+        throw ParseError("Expected '(', got " +
+                         (tokens.empty() ? "nothing" : tokens[0]));
+    }
+    std::vector<std::string>::iterator it = tokens.begin() + 1;
+    std::vector<std::string> result = _parse_list_aux(it, tokens.end());
+    if (it != tokens.end()) {
+        throw ParseError("Unexpected token: " + *it);
+    }
+    return result;
+}
+
