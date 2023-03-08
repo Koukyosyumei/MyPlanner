31c45a5441ce52bddd7bde4d8cd3aa3d3d3b20e0
Koukyosyumei <koukyosyumei@hotmail.com>
2023-03-08 11:11:57 +0900

    update

--- test/test_parser_pddl_complex.cpp
+++ test/test_parser_pddl_complex.cpp
@@ -190,4 +190,86 @@ TEST(parser_pddl_complex, DomainDef) {
     ASSERT_EQ(action.parameters[1].name, "?y");
     ASSERT_EQ(action.parameters[1].types[0], "block");
 
+    Formula pre = action.precond.formula;
+    ASSERT_EQ(pre.key, "and");
+    std::vector<std::string> test_key_1 = {"on", "clear", "handempty"};
+    for (int i = 0; i < test_key_1.size(); i++) {
+        ASSERT_EQ(pre.children[i].key, test_key_1[i]);
+    }
+    ASSERT_EQ(pre.children[0].children[0].key, "?x");
+    ASSERT_EQ(pre.children[0].children[1].key, "?y");
+    ASSERT_EQ(pre.children[1].children[0].key, "?x");
+    ASSERT_EQ(pre.children[2].children.size(), 0);
+
+    Formula eff = action.effect.formula;
+    ASSERT_EQ(eff.key, "and");
+    std::vector<std::string> test_key_2 = {"holding", "clear", "not", "not",
+                                           "not"};
+    for (int i = 0; i < test_key_2.size(); i++) {
+        ASSERT_EQ(eff.children[i].key, test_key_2[i]);
+    }
+}
+
+TEST(parser_pddl_complex, predList2) {
+    std::vector<std::string> test = {
+        "(:predicates (at ?x - (either person aircraft) ?c - city)",
+        "     (in ?p - person ?a - aircraft)",
+        "     (fuel-level ?a - aircraft ?l - flevel)",
+        "     (next ?l1 ?l2 - flevel))"};
+    LispIterator iter = parse_lisp_iterator(test);
+    PredicatesStmt pred = parse_predicates_stmt(iter);
+    std::vector<std::string> test_name_1 = {"at", "in", "fuel-level", "next"};
+    for (int i = 0; i < pred.predicates.size(); i++) {
+        ASSERT_EQ(pred.predicates[i].name, test_name_1[i]);
+    }
+    std::vector<std::string> test_name_2 = {"?x", "?p", "?a", "?l1"};
+    for (int i = 0; i < pred.predicates.size(); i++) {
+        ASSERT_EQ(pred.predicates[i].parameters[0].name, test_name_2[i]);
+    }
+    std::vector<std::string> test_name_3 = {"person", "person", "aircraft",
+                                            "flevel"};
+    for (int i = 0; i < pred.predicates.size(); i++) {
+        ASSERT_EQ(pred.predicates[i].parameters[0].types[0], test_name_3[i]);
+    }
+}
+
+TEST(parser_pddl_complex, ObjectStmt) {
+    std::vector<std::string> test = {
+        "(:objects",
+        "apn1 - airplane",
+        "apt2 apt1 - airport",
+        "pos2 pos1 - location",
+        "cit2 cit1 - city",
+        "tru2 tru1 - truck",
+        "obj23 obj22 obj21 obj13 obj12 obj11 - package)"};
+    LispIterator iter = parse_lisp_iterator(test);
+    std::vector<Object> objects = parse_objects_stmt(iter);
+    std::vector<std::string> test_names = {
+        "apn1", "apt2",  "apt1",  "pos2",  "pos1",  "cit2",  "cit1", "tru2",
+        "tru1", "obj23", "obj22", "obj21", "obj13", "obj12", "obj11"};
+    for (int i = 0; i < objects.size(); i++) {
+        ASSERT_EQ(objects[i].name, test_names[i]);
+    }
+}
+
+TEST(parser_pddl_complex, InitStmt) {
+    std::vector<std::string> test = {
+        "(:INIT (CLEAR C) (CLEAR A H) (CLEAR B) (CLEAR D) (ONTABLE C) (ONTABLE "
+        "A)",
+        "(ONTABLE B) (ONTABLE D) (HANDEMPTY))"};
+    LispIterator iter = parse_lisp_iterator(test);
+    InitStmt init = parse_init_stmt(iter);
+    std::vector<std::string> test_names = {"clear",   "clear",   "clear",
+                                           "clear",   "ontable", "ontable",
+                                           "ontable", "ontable", "handempty"};
+    for (int i = 0; i < init.predicates.size(); i++) {
+        ASSERT_EQ(init.predicates[i].name, test_names[i]);
+    }
+    std::vector<std::vector<std::string>> test_params = {
+        {"c"}, {"a", "h"}, {"b"}, {"d"}, {"c"}, {"a"}, {"b"}, {"d"}, {}};
+    for (int i = 0; i < init.predicates.size(); i++) {
+        for (int j = 0; j < init.predicates[i].parameters.size(); j++) {
+            ASSERT_EQ(init.predicates[i].parameters[j], test_params[i][j]);
+        }
+    }
 }
