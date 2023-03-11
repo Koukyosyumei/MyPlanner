ae26722469bf4fd89d3947d86aa7f46e70d66aa6
Koukyosyumei <koukyosyumei@hotmail.com>
2023-03-12 00:11:34 +0900

    update

--- test/test_tree_visitor.cpp
+++ test/test_tree_visitor.cpp
@@ -1,12 +1,13 @@
 #include <gtest/gtest.h>
 
+#include <algorithm>
 #include <cassert>
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <string>
-#include <vector>
 #include <unordered_set>
+#include <vector>
 
 #include "myplan/pddl/lisp_iterators.h"
 #include "myplan/pddl/lisp_parser.h"
@@ -43,10 +44,84 @@ TEST(parser_pddl_tree_visitor, PDDLVisitor) {
     Domain _domain = _parser.parse_domain(false);
     Problem _problem = _parser.parse_problem(_domain, false);
 
-    // test action set
+    // test action set & parameters
     std::unordered_set<std::string> test_us = {"pick-up", "put-down", "stack",
                                                "unstack"};
+
+    size_t tmp_cnt_0 = 0;
     for (auto a : _domain.actions_dict) {
         ASSERT_TRUE(test_us.find(a.first) != test_us.end());
+        for (auto s : a.second.signature) {
+            ASSERT_EQ(s.second[0].name, "block");
+            tmp_cnt_0++;
+        }
     }
+    ASSERT_EQ(tmp_cnt_0, 6);
+
+    // test action precondition
+    std::unordered_set<std::string> test_precond_names = {
+        "clear", "ontable", "handempty", "holding",  "holding",
+        "clear", "on",      "clear",     "handempty"};
+    std::unordered_set<std::string> precond_names;
+    for (auto ap : _domain.actions_dict) {
+        Action a = ap.second;
+        for (auto p : a.precondition) {
+            ASSERT_TRUE(test_precond_names.find(p.name) !=
+                        test_precond_names.end());
+            precond_names.insert(p.name);
+        }
+    }
+    ASSERT_EQ(precond_names.size(), test_precond_names.size());
+
+    // test action effects
+    std::vector<std::string> all_effects_add = {
+        "holding",   "clear", "handempty", "ontable", "clear",
+        "handempty", "on",    "holding",   "clear"};
+    std::vector<std::string> all_effects_del = {
+        "ontable", "clear", "handempty", "holding", "clear",
+        "holding", "clear", "handempty", "on"};
+    size_t tmp_cnt_2 = 0;
+    size_t tmp_cnt_3 = 0;
+    for (auto ap : _domain.actions_dict) {
+        Action a = ap.second;
+        for (auto p : a.effect.addlist) {
+            ASSERT_TRUE(std::find(all_effects_add.begin(),
+                                  all_effects_add.end(),
+                                  p.name) != all_effects_add.end());
+            tmp_cnt_2++;
+        }
+        for (auto p : a.effect.dellist) {
+            ASSERT_TRUE(std::find(all_effects_del.begin(),
+                                  all_effects_del.end(),
+                                  p.name) != all_effects_del.end());
+            tmp_cnt_3++;
+        }
+    }
+    ASSERT_EQ(tmp_cnt_2, all_effects_add.size());
+    ASSERT_EQ(tmp_cnt_3, all_effects_del.size());
+
+    // test action domain name
+    ASSERT_EQ(_domain.name, "blocks");
+
+    // test actino predicates
+    std::unordered_set<std::string> test_pred_names = {"on", "ontable", "clear",
+                                                       "handempty", "holding"};
+    std::unordered_set<std::string> pred_names;
+    for (auto p : _domain.predicates_dict) {
+        pred_names.insert(p.first);
+        for (auto s : p.second.signature) {
+            ASSERT_EQ(s.second[0].name, "block");
+        }
+    }
+    ASSERT_EQ(pred_names, test_pred_names);
+
+    // test constants
+    std::unordered_set<std::string> test_constants_key = {"horst", "block1",
+                                                          "block2"};
+    std::unordered_set<std::string> constants_key;
+    for (auto c : _domain.constants) {
+        constants_key.insert(c.first);
+        ASSERT_EQ(c.second.name, "block");
+    }
+    ASSERT_EQ(constants_key, test_constants_key);
 }
